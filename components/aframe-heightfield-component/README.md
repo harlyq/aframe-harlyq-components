e94f8636da1fa2073331dcc4d621510bff32bcde
harlyq <celerity252@gmail.com>
2019-04-26 12:14:00 +0930

    added a smooth option for heightfield
    - smooth heightfields can now be generated

--- components/aframe-heightfield-component/README.md
+++ components/aframe-heightfield-component/README.md
@@ -21,6 +21,21 @@ Which channel to extract the height information from
 Scaling of the height value.  The maximum intensity represents 1 m when scale is 1
 
 ---
+**numRows**: int = `32`
+
+The number of rows (in the z dimension) for the heightfield
+
+---
+**numCols**: int = `32`
+
+The number of cols (in the x dimension) for the heightfield
+
+---
+**smooth**: boolean = `false`
+
+If true, each height point has one vertex and one normal (the average of the triangle face normals).  If false, each triangle's vertices and normals is independent of all other triangles, and each normal matches the normal of the triangle. This uses much a lot more memory, but makes it easier to color each triangle independently and produce a faceted aesthetic.
+
+---
 **src**: selector = `null`
 
 The src image to use for the heightfield
--- components/aframe-heightfield-component/index.js
+++ components/aframe-heightfield-component/index.js
@@ -16,6 +16,7 @@ AFRAME.registerComponent("heightfield", {
     numCols: { type: "int", default: 32 },
     heightScale: { default: .2 },
     channels: { default: "rgb", oneOf: Object.keys(CHANNEL_MULTIPLIERS) },
+    smooth: { default: false }
   },
 
   init() {
@@ -30,7 +31,7 @@ AFRAME.registerComponent("heightfield", {
 
     if (oldData.src !== data.src) {
       this.loadTexture(data.src)
-    } else if (oldData.numRows !== data.numRows || oldData.numCols !== data.numCols) {
+    } else if (oldData.numRows !== data.numRows || oldData.numCols !== data.numCols || oldData.smooth !== data.smooth) {
       this.createHeightfield(this.image)
     } else if (oldData.heightScale !== data.heightScale || oldData.channels !== data.channels) {
       // check the object3D mesh to ensure we still control it
@@ -70,42 +71,52 @@ AFRAME.registerComponent("heightfield", {
 
     const numRows = this.data.numRows
     const numCols = this.data.numCols
+    let geometry
 
-    // const geometry = new THREE.PlaneBufferGeometry(1, 1, numCols - 1, numRows - 1)
-    const numCells = numCols*numRows
-    const numVerts = numCells*VERTS_PER_CELL
-    const geometry = new THREE.BufferGeometry()
-    const vertices = new Float32Array(numVerts*3)
-    const normals = new Float32Array(numVerts*3)
-    const uvs = new Float32Array(numVerts*2)
+    if (this.data.smooth) {
 
-    // (x,y)a--b  triangles are *bad* and *cda*
-    //      |\ |  vertices are *badcda*
-    //      | \|  indices are 0-2=b, 3-5=a, 6-8=d, 9-11=c, 12-14=d, 15-17=a
-    //      c--d  ax=3,15 az=5,17 bx=0 bz=2 cx=9 cz=11 dx=6,12 dz=8,14
+      geometry = new THREE.PlaneBufferGeometry(1, 1, numCols, numRows)
+      geometry.applyMatrix( new THREE.Matrix4().set(1,0,0,0, 0,0,-1,0, 0,-1,0,0, 0,0,0,1) ) // rotate -90 about x
 
-    for (let z = 0; z < numRows; z++) {
-      for (let x = 0; x < numCols; x++) {
-        const i = (z*numCols + x)*VERTS_PER_CELL*3
-        const j = (z*numCols + x)*VERTS_PER_CELL*2
-        const minU = x/numCols, maxU = (x+1)/numCols
-        const minV = z/numRows, maxV = (z+1)/numRows
+    } else {
 
-        vertices[i+3] = vertices[i+9] = vertices[i+15] = minU - .5 // ax,cx
-        vertices[i+2] = vertices[i+5] = vertices[i+17] = minV - .5 // az,bz
-        vertices[i+0] = vertices[i+6] = vertices[i+12] = maxU - .5 // bx,dx
-        vertices[i+8] = vertices[i+11] = vertices[i+14] = maxV - .5 // cz,dz
-
-        uvs[j+2] = uvs[j+6] = uvs[j+10] = minU
-        uvs[j+1] = uvs[j+3] = uvs[j+11] = 1 - minV
-        uvs[j+0] = uvs[j+4] = uvs[j+8] = maxU
-        uvs[j+5] = uvs[j+7] = uvs[j+9] = 1 - maxV
+      const numCells = numCols*numRows
+      const numVerts = numCells*VERTS_PER_CELL
+      const vertices = new Float32Array(numVerts*3)
+      const normals = new Float32Array(numVerts*3)
+      const uvs = new Float32Array(numVerts*2)
+  
+      geometry = new THREE.BufferGeometry()
+  
+      // (x,y)a--b  triangles are *bad* and *cda*
+      //      |\ |  vertices are *badcda*
+      //      | \|  indices are 0-2=b, 3-5=a, 6-8=d, 9-11=c, 12-14=d, 15-17=a
+      //      c--d  ax=3,15 az=5,17 bx=0 bz=2 cx=9 cz=11 dx=6,12 dz=8,14
+  
+      for (let z = 0; z < numRows; z++) {
+        for (let x = 0; x < numCols; x++) {
+          const i = (z*numCols + x)*VERTS_PER_CELL*3
+          const j = (z*numCols + x)*VERTS_PER_CELL*2
+          const minU = x/numCols, maxU = (x+1)/numCols
+          const minV = z/numRows, maxV = (z+1)/numRows
+  
+          vertices[i+3] = vertices[i+9] = vertices[i+15] = minU - .5 // ax,cx
+          vertices[i+2] = vertices[i+5] = vertices[i+17] = minV - .5 // az,bz
+          vertices[i+0] = vertices[i+6] = vertices[i+12] = maxU - .5 // bx,dx
+          vertices[i+8] = vertices[i+11] = vertices[i+14] = maxV - .5 // cz,dz
+  
+          uvs[j+2] = uvs[j+6] = uvs[j+10] = minU
+          uvs[j+1] = uvs[j+3] = uvs[j+11] = 1 - minV
+          uvs[j+0] = uvs[j+4] = uvs[j+8] = maxU
+          uvs[j+5] = uvs[j+7] = uvs[j+9] = 1 - maxV
+        }
       }
-    }
+  
+      geometry.addAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+      geometry.addAttribute('normal', new THREE.Float32BufferAttribute(normals, 3))
+      geometry.addAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2))
 
-    geometry.addAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
-    geometry.addAttribute('normal', new THREE.Float32BufferAttribute(normals, 3))
-    geometry.addAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2))
+    }
 
     const oldMesh = this.el.getObject3D("mesh")
     this.mesh = new THREE.Mesh(geometry, oldMesh ? oldMesh.material : new THREE.MeshBasicMaterial())
@@ -169,50 +180,56 @@ AFRAME.registerComponent("heightfield", {
       }
     }
 
-    // these values are for cells
-    for (let z = 0; z < numRows; z++) {
-      for (let x = 0; x < numCols; x++) {
-        const i = x + z*(numCols+1) // points
-        const j = x + z*numCols // cells
-        const k = j*VERTS_PER_CELL // verts
-        const heightA = heights[i]
-        const heightB = heights[i + 1]
-        const heightC = heights[i + numCols + 1]
-        const heightD = heights[i + numCols + 2]
-        const midHeight = midHeights[j]
-        const minU = x/numCols
-        const maxU = (x + 1)/numCols
+    if (this.data.smooth) {
 
-        positions.setY(k, heightB)
-        positions.setY(k+1, heightA)
-        positions.setY(k+3, heightC)
-        positions.setY(k+4, heightD)
+      for (let i = 0; i < positions.count; i++) {
+        positions.setY(i, heights[i])
+      }
 
-        // cut the square in the direction which is closest to the image midpoint height, this lessens the
-        // ridged values artefact.  Just set the U because the V doesn't change
-        if (Math.abs((heightA + heightD)*.5 - midHeight) > Math.abs((heightC + heightB)*.5 - midHeight)) {
-          // switch to *baccdb*
-          positions.setX(k+2, minU - .5)
-          positions.setY(k+2, heightC)
-          positions.setX(k+5, maxU - .5)
-          positions.setY(k+5, heightB)
-          uvs.setX(k+2, minU)
-          uvs.setX(k+5, maxU)
-        } else {
-          // output as *badcda*
-          positions.setX(k+2, maxU - .5)
-          positions.setY(k+2, heightD)
-          positions.setX(k+5, minU - .5)
-          positions.setY(k+5, heightA)
-          uvs.setX(k+2, maxU)
-          uvs.setX(k+5, minU)
+    } else {
+
+      // these values are for cells
+      for (let z = 0; z < numRows; z++) {
+        for (let x = 0; x < numCols; x++) {
+          const i = x + z*(numCols+1) // points
+          const j = x + z*numCols // cells
+          const k = j*VERTS_PER_CELL // verts
+          const heightA = heights[i]
+          const heightB = heights[i + 1]
+          const heightC = heights[i + numCols + 1]
+          const heightD = heights[i + numCols + 2]
+          const midHeight = midHeights[j]
+          const minU = x/numCols
+          const maxU = (x + 1)/numCols
+
+          positions.setY(k, heightB)
+          positions.setY(k+1, heightA)
+          positions.setY(k+3, heightC)
+          positions.setY(k+4, heightD)
+
+          // cut the square in the direction which is closest to the image midpoint height, this lessens the
+          // ridged values artefact.  Just set the U because the V doesn't change
+          if (Math.abs((heightA + heightD)*.5 - midHeight) > Math.abs((heightC + heightB)*.5 - midHeight)) {
+            // switch to *baccdb*
+            positions.setX(k+2, minU - .5)
+            positions.setY(k+2, heightC)
+            positions.setX(k+5, maxU - .5)
+            positions.setY(k+5, heightB)
+            uvs.setX(k+2, minU)
+            uvs.setX(k+5, maxU)
+          } else {
+            // output as *badcda*
+            positions.setX(k+2, maxU - .5)
+            positions.setY(k+2, heightD)
+            positions.setX(k+5, minU - .5)
+            positions.setY(k+5, heightA)
+            uvs.setX(k+2, maxU)
+            uvs.setX(k+5, minU)
+          }
         }
       }
-    }
 
-    // for (let i = 0; i < pixels.length; i += 4) {
-    //   positions.setZ(i/4, height*heightScale)
-    // }
+    }
 
     this.mesh.geometry.computeVertexNormals()
 
