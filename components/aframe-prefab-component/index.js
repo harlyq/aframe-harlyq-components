3c2234a637618af68c53fcd15814844c88979819
harlyq <celerity252@gmail.com>
2019-05-05 18:09:23 +0930

    added prefab component
    - the prefab component generates an entity hierarchy from a template, and adds it as the child of the current entity

---
+++ components/aframe-prefab-component/index.js
@@ -0,0 +1,74 @@
+import { aframeHelper } from "harlyq-helpers"
+
+AFRAME.registerComponent('prefab', {
+  schema: {
+    template: { default: "" },
+    debug: { default: false },
+  },
+
+  init() {
+    this.templateContent = undefined
+    this.hasPrefab = false
+  },
+
+  remove() {
+    this.destroyPrefab()
+  },
+
+  updateSchema(newData) {
+    if (typeof newData !== "object") {
+      console.error(`invalid properties, expected format <property>:<value>; '${newData}'`)
+    }
+    
+    const originalSchema = AFRAME.components[this.name].schema
+    let newSchema = {}
+
+    for (let prop in newData) {
+      if (!(prop in originalSchema)) {
+        newSchema[prop] = { type: "string" }
+      }
+    }
+
+    if (Object.keys(newSchema).length > 0) {
+      this.extendSchema(newSchema)
+    }
+  },
+
+  update(oldData) {
+    const data = this.data
+    if (oldData.template !== data.template) {
+      aframeHelper.loadTemplate( data.template, (text) => this.templateContent = text )
+    }
+
+    this.destroyPrefab()
+    this.createPrefab()
+  },
+
+  createPrefab() {
+    if (!this.hasPrefab) {
+      const newHTML = this.processTemplate(this.templateContent)
+      this.el.innerHTML = newHTML
+      this.hasPrefab = true
+
+      if (this.data.debug) {
+        console.log(newHTML)
+      }
+    }
+  },
+
+  destroyPrefab() {
+    if (this.hasPrefab) {
+      while (this.el.lastChild) {
+        this.el.removeChild(this.el.lastChild)
+      }
+      this.hasPrefab = false
+    }
+  },
+
+  processTemplate(str) {
+    const templateArgs = Object.keys(this.data).concat("return `" + str + "`")
+    const fn = new Function(...templateArgs)
+    // @ts-ignore
+    return fn(...Object.values(this.data))
+  },
+})
