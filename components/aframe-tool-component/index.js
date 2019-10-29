8653c98fc0a036a82d8a4f5ceed7484231020490
harlyq <celerity252@gmail.com>
2019-10-29 14:37:20 +1030

    toy supports two handed control

--- components/aframe-tool-component/index.js
+++ components/aframe-tool-component/index.js
@@ -12,15 +12,18 @@ AFRAME.registerComponent("tool", {
     handPosition: { type: "vec3" },
     handRotation: { type: "vec3" },
     handScale: { type: "vec3", default: {x:1, y:1, z:1} },
+    maxHands: { default: 1 },
     usage: { default: "stayondrop", oneOf: ["respawnOnDrop", "stayOnDrop"], parse: parseToLowerCase },
   },
 
   init() {
-    this.handA = undefined
+    this.hands = []
     this.onGrabStart = this.onGrabStart.bind(this)
     this.onGrabEnd = this.onGrabEnd.bind(this)
-    this.handMatrix = new THREE.Matrix4()
+    this.offsetMatrix = new THREE.Matrix4()
     this.objectMatrixOnEquip = new THREE.Matrix4()
+    this.originalScale = new THREE.Vector3()
+    this.invRotationMatrix = new THREE.Matrix4()
 
     const system = this.el.sceneEl.systems["grab-system"]
     system.registerTarget(this.el, 1)
@@ -42,50 +45,81 @@ AFRAME.registerComponent("tool", {
     if (oldData.handRotation !== data.handRotation || oldData.handPosition !== data.handPosition || oldData.handScale !== data.handScale) {
       const euler = new THREE.Euler().set(degToRad(data.handRotation.x), degToRad(data.handRotation.y), degToRad(data.handRotation.z), "YXZ")
       const quaternion = new THREE.Quaternion().setFromEuler(euler)
-      this.handMatrix.compose(data.handPosition, quaternion, data.handScale)
+      this.offsetMatrix.compose(data.handPosition, quaternion, data.handScale)
+      this.invRotationMatrix.makeRotationFromQuaternion(quaternion)
+      this.invRotationMatrix.getInverse(this.invRotationMatrix)
     }
   },
 
   tick: (function() {
     const newMatrix = new THREE.Matrix4()
     const inverseParentMat = new THREE.Matrix4()
+    const forwardVector = new THREE.Vector3(0,0,1)
+    const secondVector = new THREE.Vector3()
+    const secondHandPosition = new THREE.Vector3()
+    const firstHandPosition = new THREE.Vector3()
+    const twoHandQuaternion = new THREE.Quaternion()
+    const twooffsetMatrix = new THREE.Matrix4()
 
     return function tick() {
-      if (!this.handA) {
+      if (this.hands.length === 0) {
         this.el.sceneEl.removeBehavior(this)
         return
       }
   
-      if (this.handA) {
-        const object3D = this.el.object3D
-        const hand3D = this.handA.object3D
+      const object3D = this.el.object3D
+      const hand3D = this.hands[0].object3D
 
-        hand3D.updateMatrixWorld(true)
-        object3D.parent.updateMatrixWorld(true)
+      hand3D.updateMatrixWorld(true)
+      object3D.parent.updateMatrixWorld(true)
 
-        // get the inverse each frame in case the parent is moving
-        inverseParentMat.getInverse(object3D.parent.matrixWorld)
-        newMatrix.copy(this.handMatrix).premultiply(hand3D.matrixWorld).premultiply(inverseParentMat)
-        newMatrix.decompose(object3D.position, object3D.quaternion, object3D.scale)
+      // get the inverse each frame in case the parent is moving
+      inverseParentMat.getInverse(object3D.parent.matrixWorld)
+
+      if (this.hands.length > 1) {
+        const secondHand3D = this.hands[1].object3D
+        secondHand3D.updateMatrixWorld(true)
+        secondHand3D.getWorldPosition(secondHandPosition)
+        hand3D.getWorldPosition(firstHandPosition)
+        secondVector.subVectors(firstHandPosition, secondHandPosition).normalize()
+        forwardVector.set(0,0,1)
+        twoHandQuaternion.setFromUnitVectors(forwardVector, secondVector)
+        twooffsetMatrix.makeRotationFromQuaternion(twoHandQuaternion)
+        twooffsetMatrix.multiply(this.invRotationMatrix)
+        twooffsetMatrix.setPosition(firstHandPosition)
+        newMatrix.copy(this.offsetMatrix).premultiply(twooffsetMatrix).premultiply(inverseParentMat)
+      } else {
+        newMatrix.copy(this.offsetMatrix).premultiply(hand3D.matrixWorld).premultiply(inverseParentMat)
       }
+
+      newMatrix.decompose(object3D.position, object3D.quaternion, object3D.scale)
+      object3D.scale.multiply(this.originalScale)
     }
   })(),
 
   onGrabStart(e) {
+    const position = new THREE.Vector3()
+    const quaternion = new THREE.Quaternion()
     const data = this.data
 
-    this.handA = e.detail.hand
+    this.hands.length = Math.min(data.maxHands - 1, this.hands.length)
+    this.hands.push(e.detail.hand)
     this.objectMatrixOnEquip.copy(this.el.object3D.matrix)
+    this.el.object3D.matrix.decompose(position, quaternion, this.originalScale)
     this.el.sceneEl.addBehavior(this)
   },
 
   onGrabEnd(e) {
     const data = this.data
     const object3D = this.el.object3D
+    const handIndex = this.hands.indexOf(e.detail.hand)
 
-    this.handA = undefined
-    if (data.usage === "respawnondrop") {  
-      this.objectMatrixOnEquip.decompose(object3D.position, object3D.quaternion, object3D.scale)
+    if (handIndex !== -1) {
+      this.hands.splice(handIndex, 1)
+
+      if (this.hands.length === 0 && data.usage === "respawnondrop") {
+        this.objectMatrixOnEquip.decompose(object3D.position, object3D.quaternion, object3D.scale)
+      }
     }
   },
 })
