52179bc8302fe805ee6552c0efcd80aa7425b0fb
harlyq <celerity252@gmail.com>
2020-01-11 13:06:04 +1030

    moved 3D card functions into harlyq-helpers/cardHelper

--- components/aframe-card-pack-component/index.js
+++ components/aframe-card-pack-component/index.js
@@ -1,107 +1,4 @@
-import { aframeHelper, domHelper, instanced, utils } from "harlyq-helpers"
-
-function createCardMesh(width, height, texture) {
-  const w_2 = width/2, h_2 = height/2
-  const positions = new Float32Array([
-    -w_2, 0,  h_2,
-     w_2, 0,  h_2,
-     w_2, 0, -h_2,
-    -w_2, 0, -h_2,
-     w_2, 0,  h_2,
-    -w_2, 0,  h_2,
-    -w_2, 0, -h_2,
-     w_2, 0, -h_2,
-  ])
-  const normals = new Float32Array([
-    0,1,0,
-    0,1,0,
-    0,1,0,
-    0,1,0,
-    0,-1,0,
-    0,-1,0,
-    0,-1,0,
-    0,-1,0,
-  ])
-  const uvs = new Float32Array([
-    0,1,
-    1,1,
-    1,0,
-    0,0,
-    0,1,
-    1,1,
-    1,0,
-    0,0,
-  ])
-  const indices = [
-    0,2,1,
-    0,3,2,
-    4,6,5,
-    4,7,6,
-  ]
-
-  const cardMaterial = new THREE.MeshStandardMaterial({ map: texture })
-  const cardGeo = new THREE.BufferGeometry()
-  cardGeo.setAttribute("position", new THREE.BufferAttribute(positions, 3))
-  cardGeo.setAttribute("normal", new THREE.BufferAttribute(normals, 3))
-  cardGeo.setAttribute("uv", new THREE.BufferAttribute(uvs, 2))
-  cardGeo.setIndex(indices)
-
-  return new THREE.Mesh(cardGeo, cardMaterial)
-}
-
-function createPack(numCards, texture, textureRows, textureCols, width, height) {
-  const cardObj3D = createCardMesh(width, height, texture)
-  const packInstancedMesh = instanced.createMesh(cardObj3D, numCards)
-
-  packInstancedMesh.geometry.setAttribute( "instanceCardFrame", new THREE.InstancedBufferAttribute (new Float32Array(numCards*2), 2 ) )
-
-  packInstancedMesh.material.onBeforeCompile = (function(oldBeforeCompile) {
-    return function beforeCompileForPack(shader, renderer) {
-      oldBeforeCompile(shader, renderer)
-
-      shader.uniforms.textureFrames = { value: new THREE.Vector2(textureCols, textureRows) }
-
-      shader.vertexShader = shader.vertexShader.replace("void main()", `
-      attribute vec2 instanceCardFrame;
-      uniform vec2 textureFrames;
-
-      void main()`)
-
-      shader.vertexShader = shader.vertexShader.replace("#include <uv_vertex>", `
-      #include <uv_vertex>
-      {
-      #ifdef USE_UV
-        float frame = normal.y < 0. ? instanceCardFrame.x : instanceCardFrame.y;
-        vUv.x = (mod(frame,textureFrames.x) + vUv.x)/textureFrames.x;
-        vUv.y = 1. - (floor(frame/textureFrames.x) + (1. - vUv.y))/textureFrames.y;
-      #endif
-      }`)
-    }
-  })(packInstancedMesh.material.onBeforeCompile)
-
-  packInstancedMesh.material.needsUpdate = true
-
-  return {
-    packInstancedMesh,
-    nextIndex: 0,
-    numCards,
-  }
-}
-
-function createCard(pack, front, back) {
-  const index = pack.nextIndex++
-  pack.packInstancedMesh.geometry.maxInstancedCount = index + 1
-
-  setCardFrameAt(pack.packInstancedMesh, index, front, back)
-  instanced.setScaleAt(pack.packInstancedMesh, index, 1, 1, 1)
-
-  return index
-}
-
-function setCardFrameAt(instancedMesh, index, front, back) {
-  const cardFrame = instancedMesh.geometry.getAttribute("instanceCardFrame")
-  cardFrame.setXY(index, front, back)
-}
+import { aframeHelper, cardHelper, domHelper, instanced, utils } from "harlyq-helpers"
 
 AFRAME.registerComponent("card-pack", {
   schema: {
@@ -188,13 +85,13 @@ AFRAME.registerComponent("card-pack", {
     const data = this.data
     const pos = new THREE.Vector3()
 
-    this.pack = createPack(data.numCards, null, data.textureRows, data.textureCols, data.width, data.height )
+    this.pack = cardHelper.createPack(data.numCards, null, data.textureRows, data.textureCols, data.width, data.height )
 
     const instancedMesh = this.pack.packInstancedMesh
     this.el.object3D.add(instancedMesh)
 
     for (let i = 0; i < data.numCards; i++) {
-      const index = createCard(this.pack, data.frontStart + i, data.back)
+      const index = cardHelper.createCard(this.pack, data.frontStart + i, data.back)
 
       pos.set(0, i*data.zOffset, 0)
       instanced.setPositionAt(instancedMesh, index, pos)
